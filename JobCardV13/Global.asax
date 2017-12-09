1347f7c04feb3f5b39c6e15634900da864b3d796
Laurence Cramp <larcramp@yahoo.ie>
2017-12-09 10:50:05 +0000

    Add project files.

---
+++ JobCardV13/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="JobCardV13.MvcApplication" Language="C#" %>
---
+++ JobCardV13/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(JobCardV13.Startup))]
+namespace JobCardV13
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
